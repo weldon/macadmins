05a8fed273ead4b75e4e933994557f5e986541de
weldon <weldon@dodd.org>
2020-04-27 13:16:56 -0600

    added support for sites and updated variable names

--- update_computer_smartgroup.py
+++ update_computer_smartgroup.py
@@ -23,34 +23,36 @@ def main():
 	app_version = "4.6.12"
 	
 	#### Jamf Pro Server variables ####
+	# site id, leave this set to "-1" and "None" if it doesn't belong in a site
+	site_id = "-1"
+	site_name = "None"
 	# you can optionally hard code the url, username, and password for your Jamf Pro Server
 	# if left empty, the script will prompt the user for the values at runtime
-	base_url = ""
-	jssuser = ""
-	jsspass = ""
+	jps_url = ""
+	jpsuser = ""
+	jpspass = ""
 	
 	# base URL of JSS
-	if base_url == "":			
+	if jps_url == "":			
 		print('Enter your Jamf Pro Server hostname in the form "https://servername.com:8443"')
-		base_url = input('Jamf Pro Server URL: ')
-	api_url = base_url + '/JSSResource/computergroups/id/{id}'.format(id = sg_id)
-
-		
+		jps_url = input('Jamf Pro Server URL: ')
+	api_url = jps_url + '/JSSResource/computergroups/id/{id}'.format(id = sg_id)
+	
 	# JSS API credentials
-	if jssuser == "":
-		jssuser = input('Enter your Jamf Pro username: ')
-	if jsspass == "":
-		jsspass = getpass.getpass('Enter your Jamf Pro password: ')	
+	if jpsuser == "":
+		jpsuser = input('Enter your Jamf Pro username: ')
+	if jpspass == "":
+		jpspass = getpass.getpass('Enter your Jamf Pro password: ')	
 	
 	# XML Payload
-	payload = '<?xml version=\"1.0\" encoding=\"UTF-8\"?><computer_group><id>{id}</id><name>{name}</name><is_smart>true</is_smart><site><id>-1</id><name>None</name></site><criteria><size>2</size><criterion><name>Application Title</name><priority>0</priority><and_or>and</and_or><search_type>{name_type}</search_type><value>{app}</value><opening_paren>false</opening_paren><closing_paren>false</closing_paren></criterion><criterion><name>Application Version</name><priority>1</priority><and_or>and</and_or><search_type>{version_type}</search_type><value>{version}</value><opening_paren>false</opening_paren><closing_paren>false</closing_paren></criterion></criteria></computer_group>'.format(id=sg_id, name=sg_name, name_type=name_search_type, app=app_title, version_type=version_search_type, version=app_version)
+	payload = '<?xml version=\"1.0\" encoding=\"UTF-8\"?><computer_group><id>{id}</id><name>{name}</name><is_smart>true</is_smart><site><id>{site}</id><name>{site_name}</name></site><criteria><size>2</size><criterion><name>Application Title</name><priority>0</priority><and_or>and</and_or><search_type>{name_type}</search_type><value>{app}</value><opening_paren>false</opening_paren><closing_paren>false</closing_paren></criterion><criterion><name>Application Version</name><priority>1</priority><and_or>and</and_or><search_type>{version_type}</search_type><value>{version}</value><opening_paren>false</opening_paren><closing_paren>false</closing_paren></criterion></criteria></computer_group>'.format(id=sg_id, name=sg_name, site=site_id, site_name=site_name, name_type=name_search_type, app=app_title, version_type=version_search_type, version=app_version)
 		
 	# Request
-	try: r  = requests.put(api_url, headers={'Accept': 'application/xml'}, auth=(jssuser,jsspass), data=payload)
+	try: r  = requests.put(api_url, headers={'Accept': 'application/xml'}, auth=(jpsuser,jpspass), data=payload)
 	except:
-		print("something went wrong with your connection to the JPS")
+		print("something went wrong with your connection to {jps}".format(jps=jps_url))
 	else:
-		print("connection to JPS was successful")
+		print("connection to {jps} was successful".format(jps=jps_url))
 	
 	# Check if smart group was updated
 	if r.status_code == 201:
